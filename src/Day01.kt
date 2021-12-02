73906980d787d12ae3c14301a2b4f68ae4b9ad16
Almar <almar.schouten@bottomline.eu>
2021-12-02 15:19:43 +0100

    Add Day01 JetBrains walk through solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,6 +9,11 @@ fun main() {
         return result
     }
 
+    // From JetBrains walk through:
+    fun part1b(input: List<Int>): Int {
+        return input.windowed(2).count { (a, b) -> a < b }
+    }
+
     // Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
     fun part2(input: List<Int>): Int {
         if (input.size < 3) {
@@ -26,12 +31,25 @@ fun main() {
         return part1(sums)
     }
 
+    // From JetBrains walk through:
+    fun part2b(input: List<Int>): Int {
+        return input.windowed(3).windowed(2).count { (a, b) -> a.sum() < b.sum() }
+    }
+
+    // From JetBrains walk through, iteration 2:
+    // (A + B + C) < (B + C + D) ==> A < D
+    fun part2c(input: List<Int>): Int {
+        return input.windowed(4).count { it[0] < it[3] }
+    }
+
+
     // test if implementation meets criteria from the description, like:
     val testInput1 = readInput("Day01_1_test").map { it.toInt() }
     check(part1(testInput1) == 7)
 
     val input1 = readInput("Day01_1").map { it.toInt() }
     println("part 1: ${part1(input1)}")
+    println("part 1b: ${part1b(input1)}")
 
     // test if implementation meets criteria from the description, like:
     val testInput2 = readInput("Day01_2_test").map { it.toInt() }
@@ -39,4 +57,6 @@ fun main() {
 
     val input2 = readInput("Day01_2").map { it.toInt() }
     println("part 2: ${part2(input2)}")
+    println("part 2b: ${part2b(input2)}")
+    println("part 2c: ${part2c(input2)}")
 }
