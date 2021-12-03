21bd3436388ae312fb29f5dea3c86baf42a7103d
Almar <almar.schouten@bottomline.eu>
2021-12-03 23:01:32 +0100

    Add Day02 JetBrains walk through solutions

--- src/Day02.kt
+++ src/Day02.kt
@@ -48,15 +48,54 @@ fun main() {
         return Triple(x, y, aim)
     }
 
+    // ========================================================================
+    // From JetBrains walk through:
+    // ========================================================================
+    data class Operation(val direction: String, val amount: Int)
+
+    fun part1b(input: List<String>): Int {
+        var depth = 0
+        var horizontalPosition = 0
+        val operations = input.map { it.split(' ') }.map { Operation(it[0], it[1].toInt()) }
+        for ((direction, amount) in operations) {
+            when(direction) {
+                "up" -> depth -= amount
+                "down" -> depth += amount
+                "forward" -> horizontalPosition += amount
+            }
+        }
+        return depth * horizontalPosition
+    }
+
+    fun part2b(input: List<String>): Int {
+        var aim = 0
+        var depth = 0
+        var horizontalPosition = 0
+        val operations = input.map { it.split(' ') }.map { Operation(it[0], it[1].toInt()) }
+        for ((direction, amount) in operations) {
+            when(direction) {
+                "up" -> aim -= amount
+                "down" -> aim += amount
+                "forward" -> {
+                    horizontalPosition += amount
+                    depth += amount * aim
+                }
+            }
+        }
+        return depth * horizontalPosition
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput1 = readInput("Day02_test")
     check(part1(testInput1).let { pair -> pair.first * pair.second} == 150)
 
     val input1 = readInput("Day02")
     println("part 1: ${part1(input1).let { pair -> pair.first * pair.second}}")
+    println("part 1b: ${part1b(input1)}")
 
     // test if implementation meets criteria from the description, like:
     check(part2(testInput1).let { triple -> triple.first * triple.second} == 900)
 
     println("part 2: ${part2(input1).let { triple -> triple.first * triple.second}}")
+    println("part 2b: ${part2b(input1)}")
 }
